0346588c6ec46d78944962e8133689a8175e3b9c
K Sri Charvitha <charvitha.kurra@gmail.com>
2014-04-23 19:32:29 +0530

    indentation fixed

--- insertion.py
+++ insertion.py
@@ -2,5 +2,4 @@ COMMA = ','
 SQ = "'"
 for line in open("filename"):
 	rollnumber , name , phonenum , bid = line.strip().split(COMMA)
-	print "insert into table_name values(rollnumber = " + SQ + rollnumber + SQ + COMMA +"name = " + SQ + name +SQ + COMMA  + 
-	"phonenum = "+ phonenum + COMMA +"bid = "+ bid +")"
+	print "insert into table_name values(rollnumber = " + SQ + rollnumber +SQ + COMMA +"name = " +SQ+name+SQ+ COMMA + "phonenum = "+phonenum+ COMMA +"bid = "+bid+")"
